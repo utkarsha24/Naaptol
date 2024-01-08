29a2ce6de0bfee3564b07a9f19a3f323a3c3cfc5
utkarsha24 <utkarshakedari24@gmail.com>
2024-01-09 00:12:21 +0530

    updated with TestNg

--- src/main/java/pom/AddProductfromDescriptionPage.java
+++ src/main/java/pom/AddProductfromDescriptionPage.java
@@ -1,45 +1,26 @@
 package pom;
 
 import java.time.Duration;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-
-import org.bouncycastle.operator.AADProcessor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.github.dockerjava.api.model.Driver;
 
 public class AddProductfromDescriptionPage {
 	
-	@FindBy (xpath = "//img[@class='square']")private WebElement productslist;
+	
 	@FindBy (xpath = "//span[text()='Click here to Buy']")private WebElement buybutton;
 
 	
 	public AddProductfromDescriptionPage(WebDriver driver)
 	{
 		PageFactory.initElements(driver, this);
-	}
 	
-	public void ClikOnProduct(WebDriver driver, int product)
-	{
-		Set<String> a= driver.getWindowHandles();
-		Iterator <String> a1=a.iterator();
-		driver.switchTo().window(a1.next());
-		//driver.switchTo().window(a1.next());
-		System.out.println(driver.getTitle());
-		productslist.click();
 	
 	}
-	public void clikOnBuyButton(WebDriver driver)	{
-		
-		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10000));
-		wait.until(ExpectedConditions.visibilityOf(buybutton));
+	public void clikOnBuyButton()	{
+	
 		buybutton.click();
 	
 	}
--- src/main/java/pom/ProductResultPage.java
+++ src/main/java/pom/ProductResultPage.java
@@ -8,10 +8,13 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class ProductResultPage {
+import net.bytebuddy.asm.MemberSubstitution.FieldValue;
+
+public class ProductResultPage extends BasePage {
 	
 	@FindBy (xpath = "//div[@class='grid_Square ']")private List<WebElement> products;
 	@FindBy (xpath = "//a[@class='bt_compare icon chat quickFancyBox']")private List<WebElement> quickView;
+	@FindBy (xpath = "//div[@class='item_title']")private List<WebElement>productTitle;
 	public ProductResultPage(WebDriver driver)
 	{
 		PageFactory.initElements(driver, this);
@@ -30,6 +33,13 @@ public class ProductResultPage {
 		quickView.get(product).click();
 	}
 	
-	
+	public void clickOnProduct(int product)
+	{
+		products.get(product).click();
+	}
+	public String getProductTitle(int product)
+	{
+		return productTitle.get(product).getText();
+	}
 	
 }
